282c03ea256d5c5aeb91e471d327f29c8d5fa655
Kausar Ahmed <kausarahmedpial@gmail.com>
2018-07-04 17:41:06 +0600

    Initial Release

---
+++ README.md
@@ -0,0 +1,36 @@
+# _Infinity_ for Hugo
+![screenshot](https://cloud.githubusercontent.com/assets/10640964/21547903/299b8ffa-ce13-11e6-9a63-f4b941313b49.jpg "Home of the website")
+
+Infinity is a responsive, modern and clean free construction/coming soon HTML template powered by Bootstrap4. This minimal template is packed with a countdown timer, contact form, about section where you can write a little bit about yourself and also feature section.
+
+Infinity based on Bootstrap 4 and offers Amazing design layout.
+
+## Usage
+
+Create a new Hugo site by typing
+
+```
+  $ hugo new site <SITENAME>
+```
+
+Then clone this repository:
+
+```
+  $ cd <SITENAME>/themes
+  $ git clone https://github.com/themefisher/infinity-hugo
+```
+
+Now take a look at the exampleSite folder and you're ready to go!
+
+## License
+
+This Template is released under [Creative Commons Attribution 3.0 (CC-BY-3.0) License](https://creativecommons.org/licenses/by/3.0/)
+If you want to remove the credit simply make a [donation](https://www.paypal.me/Themefisher), so that we can run our contribution to hugo community.
+
+## About Themefisher
+
+[Themefisher] has been creating beautiful free and premium Bootstrap and HTML5 templates since 2014.
+Visit us at https://themefisher.com/ to see other great-looking themes.
+
+[Hugo]: https://gohugo.io/
+[Themefisher]: https://themefisher.com/
---
+++ static/js/main.js
@@ -0,0 +1,109 @@
+$(window).load(function() {
+    $("#preloader").fadeOut("slow");
+});
+
+$(document).ready(function(){
+
+
+     new WOW().init();
+
+
+     $('#top-nav').onePageNav({
+        currentClass: 'current',
+        changeHash: true,
+        scrollSpeed: 1200
+    });
+
+     
+    //animated header class
+    $(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+     //console.log(scroll);
+    if (scroll > 200) {
+        //console.log('a');
+        $(".navigation").addClass("animated");
+    } else {
+        //console.log('a');
+        $(".navigation").removeClass("animated");
+    }});
+    
+    $year = $('#countdown_dashboard').data('year');
+    $month = $('#countdown_dashboard').data('month');
+    $day = $('#countdown_dashboard').data('day');
+    $('#countdown_dashboard').countDown({
+        targetDate: {
+            'day':      $day,
+            'month':    $month,
+            'year':     $year,
+            'hour':     23,
+            'min':      59,
+            'sec':      59,
+        },
+        omitWeeks: true
+    });
+
+    $(".about-slider").owlCarousel(
+        {
+        singleItem: true,
+        pagination : true,
+        autoPlay : 5000,
+        }
+    );
+
+    //contact form validation
+    $("#contact-form").validate({
+        rules: {
+            name: {
+                required: true,
+                minlength: 2
+            },
+            message: {
+                required: true,
+                minlength: 2
+            },
+            email: {
+                required: true,
+                email: true
+            }
+        },
+        messages: {
+            name: {
+                required: "Please enter Your Name",
+                minlength: "Your name must consist of at least 2 characters"
+            },
+            message: {
+                required: "Please Write Something",
+                minlength: "Your message must consist of at least 2 characters"
+            },
+            email: "Please enter a valid email address"
+        },
+        submitHandler: function(form) {
+            $(form).ajaxSubmit({
+                type:"POST",
+                data: $(form).serialize(),
+                url:"mail.php",
+                success: function() {
+                    $('#contact-form :input').attr('disabled', 'disabled');
+                    $('#contact-form').fadeTo( "slow", 0.15, function() {
+                        $(this).find(':input').attr('disabled', 'disabled');
+                        $(this).find('label').css('cursor','default');
+                        $('#success').fadeIn();
+                    });
+                },
+                error: function() {
+                    $('#contact-form').fadeTo( "slow", 0.15, function() {
+                        $('#error').fadeIn();
+                    });
+                }
+            });
+        }
+    });
+
+});
+
+
+
+
+
+
+
