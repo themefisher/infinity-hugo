5da3db9e6f33f6bda1705982261276fa343f32d0
somrat <monnaf37@gmail.com>
2019-10-21 16:48:18 +0600

    modified contact form

--- static/js/main.js
+++ static/js/main.js
@@ -50,55 +50,6 @@ $(document).ready(function(){
         }
     );
 
-    //contact form validation
-    $("#contact-form").validate({
-        rules: {
-            name: {
-                required: true,
-                minlength: 2
-            },
-            message: {
-                required: true,
-                minlength: 2
-            },
-            email: {
-                required: true,
-                email: true
-            }
-        },
-        messages: {
-            name: {
-                required: "Please enter Your Name",
-                minlength: "Your name must consist of at least 2 characters"
-            },
-            message: {
-                required: "Please Write Something",
-                minlength: "Your message must consist of at least 2 characters"
-            },
-            email: "Please enter a valid email address"
-        },
-        submitHandler: function(form) {
-            $(form).ajaxSubmit({
-                type:"POST",
-                data: $(form).serialize(),
-                url:"mail.php",
-                success: function() {
-                    $('#contact-form :input').attr('disabled', 'disabled');
-                    $('#contact-form').fadeTo( "slow", 0.15, function() {
-                        $(this).find(':input').attr('disabled', 'disabled');
-                        $(this).find('label').css('cursor','default');
-                        $('#success').fadeIn();
-                    });
-                },
-                error: function() {
-                    $('#contact-form').fadeTo( "slow", 0.15, function() {
-                        $('#error').fadeIn();
-                    });
-                }
-            });
-        }
-    });
-
 });
 
 
