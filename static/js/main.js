cded3b01648349b2c031eb6f325a99a191e72324
somratpro <abdulmonnafsomrat@gmail.com>
2020-08-19 09:00:44 +0600

    fix about slider when coundown is off

--- static/js/main.js
+++ static/js/main.js
@@ -1,60 +1,54 @@
-$(window).load(function() {
+$(window).load(function () {
     $("#preloader").fadeOut("slow");
 });
 
-$(document).ready(function(){
+$(document).ready(function () {
 
 
-     new WOW().init();
+    new WOW().init();
 
 
-     $('#top-nav').onePageNav({
+    $('#top-nav').onePageNav({
         currentClass: 'current',
         changeHash: true,
         scrollSpeed: 1200
     });
 
-     
+
     //animated header class
-    $(window).scroll(function() {    
-    var scroll = $(window).scrollTop();
-     //console.log(scroll);
-    if (scroll > 200) {
-        //console.log('a');
-        $(".navigation").addClass("animated");
-    } else {
-        //console.log('a');
-        $(".navigation").removeClass("animated");
-    }});
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+        //console.log(scroll);
+        if (scroll > 200) {
+            //console.log('a');
+            $(".navigation").addClass("animated");
+        } else {
+            //console.log('a');
+            $(".navigation").removeClass("animated");
+        }
+    });
+
     
+
+    $(".about-slider").owlCarousel({
+        singleItem: true,
+        pagination: true,
+        autoPlay: 5000,
+    });
+
     $year = $('#countdown_dashboard').data('year');
     $month = $('#countdown_dashboard').data('month');
     $day = $('#countdown_dashboard').data('day');
     $('#countdown_dashboard').countDown({
         targetDate: {
-            'day':      $day,
-            'month':    $month,
-            'year':     $year,
-            'hour':     23,
-            'min':      59,
-            'sec':      59,
+            'day': $day,
+            'month': $month,
+            'year': $year,
+            'hour': 23,
+            'min': 59,
+            'sec': 59,
         },
         omitWeeks: true
     });
 
-    $(".about-slider").owlCarousel(
-        {
-        singleItem: true,
-        pagination : true,
-        autoPlay : 5000,
-        }
-    );
-
 });
-
-
-
-
-
-
-
